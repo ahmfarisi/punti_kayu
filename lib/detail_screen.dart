58392cf1b0bdb36168a8677137810d8b8b56f213
Nur Rachmat <ahmfarisi@gmail.com>
2025-10-22 11:14:17 +0700

    Update main and detail_screen

--- lib/detail_screen.dart
+++ lib/detail_screen.dart
@@ -6,7 +6,25 @@ class DetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: Column(
+        children: [
+          // Judul
+          Container(
+            margin: EdgeInsets.all(8),
+            child: const Text(
+              'Punti Kayu',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20
+              ),
+            ),
+          ),
+          // Icon dan Text
+          Container(),
+          // Deskripsi
+          Container()
+        ],
+      ),
     );
   }
 }
