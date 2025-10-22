328344b45d4c63bbb93a20b4d3dfd08b5a39a086
Nur Rachmat <ahmfarisi@gmail.com>
2025-10-22 11:26:35 +0700

    Update detail_screen.dart

--- lib/detail_screen.dart
+++ lib/detail_screen.dart
@@ -7,20 +7,52 @@ class DetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           // Judul
           Container(
             margin: EdgeInsets.all(8),
             child: const Text(
-              'Punti Kayu',
+              'PUNTI KAYU',
+              textAlign: TextAlign.center,
               style: TextStyle(
                 fontWeight: FontWeight.bold,
-                fontSize: 20
+                fontSize: 24
               ),
             ),
           ),
           // Icon dan Text
-          Container(),
+          Container(
+            margin: EdgeInsets.all(16),
+            child: Row(
+              children: [
+                // Icon Pertama
+                Column(
+                  children: [
+                    Icon(Icons.calendar_today),
+                    SizedBox(height: 8),
+                    Text('Buka Setiap Hari')
+                  ],
+                ),
+                // Icon Kedua
+                Column(
+                  children: [
+                    Icon(Icons.timer),
+                    SizedBox(height: 8),
+                    Text('09.00 - 16.00')
+                  ],
+                ),
+                // Icon Ketiga
+                Column(
+                  children: [
+                    Icon(Icons.money),
+                    SizedBox(height: 8),
+                    Text('Rp 15.000')
+                  ],
+                ),
+              ],
+            ),
+          ),
           // Deskripsi
           Container()
         ],
